5bde6785c0db2b79ea7fbe145205b4e80f643c9d
shehata412 <shehata412@aucegypt.edu>
2024-02-04 12:46:23 +0200

    add AI to distinguish tickets on create

--- src/db/models/index.ts
+++
@@ -1,7 +0,0 @@
-import {User} from './user';
-import {Ticket} from './ticket';
-
-User.hasMany(Ticket, {foreignKey: 'user_id'});
-Ticket.belongsTo(User, {foreignKey: 'user_id'});
-
-export { User, Ticket };
